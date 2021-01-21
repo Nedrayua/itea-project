85a6a565efd09ca1df8bae6342a68a88a8606d08
Nedrayua <nedrayua@gmail.com>
2021-01-21 09:19:36 +0200

    upadte

--- shop/api/app_restful.py
+++ shop/api/app_restful.py
@@ -5,6 +5,7 @@ from . resources import UserResource, CategoryResource, ProductResource, OrderRe
 
 app_api = Blueprint('app_api', __name__)
 api = Api(app_api)
+api.init_app(app_api)
 
 api.add_resource(UserResource, '/user', '/user/<string:id>')
 api.add_resource(ProductResource, '/product', '/product/<string:id>')
@@ -12,4 +13,4 @@ api.add_resource(CategoryResource, '/category', '/category/<string:id>')
 api.add_resource(OrderResource, '/order', '/order/<string:id>')
 api.add_resource(NewsResource, '/news', '/news/<string:id>')
 
-api.init_app(app_api)
+
