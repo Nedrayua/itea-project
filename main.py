b5b728f1ab5be80e7056231d8396c0f822938a3a
Nedrayua <nedrayua@gmail.com>
2021-01-21 10:14:23 +0200

    upadte

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import time
 from flask import Flask
 
-from shop.bot.shop_bot import bot, app
-from shop.api.app_restful import app as app2
+#from shop.bot.shop_bot import bot, app
+from shop.api.app_restful import app
 from shop.bot.config import WEBHOOK_URL
 
-bot.remove_webhook()
-time.sleep(0.5)
-bot.set_webhook(WEBHOOK_URL, certificate=open('webhook_cert.pem'))
+# bot.remove_webhook()
+# time.sleep(0.5)
+# bot.set_webhook(WEBHOOK_URL, certificate=open('webhook_cert.pem'))
 
 # app = Flask(__name__)
 # app.register_blueprint(app_api)
 # app.register_blueprint(app_tg)
 
 # app.run(debug=True)
-app2.run(debug=True)
+app.run(debug=True)
